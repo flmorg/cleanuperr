3344831973591bc305e324badcf869eeca95cf4d
Flaminel <flm.marius@gmail.com>
2025-01-01 16:08:52 +0200

    added support for Lidarr

---
+++ code/Infrastructure/Verticals/Arr/LidarrClient.cs
@@ -0,0 +1,149 @@
+﻿using System.Text;
+using Common.Configuration.Arr;
+using Common.Configuration.Logging;
+using Common.Configuration.QueueCleaner;
+using Domain.Models.Arr;
+using Domain.Models.Arr.Queue;
+using Domain.Models.Lidarr;
+using Infrastructure.Verticals.ItemStriker;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Verticals.Arr;
+
+public sealed class LidarrClient : ArrClient
+{
+    public LidarrClient(
+        ILogger<LidarrClient> logger,
+        IHttpClientFactory httpClientFactory,
+        IOptions<LoggingConfig> loggingConfig,
+        IOptions<QueueCleanerConfig> queueCleanerConfig,
+        Striker striker
+    ) : base(logger, httpClientFactory, loggingConfig, queueCleanerConfig, striker)
+    {
+    }
+    
+    protected override string GetQueueUrlPath(int page)
+    {
+        return $"/api/v1/queue?page={page}&pageSize=200&includeUnknownArtistItems=true&includeArtist=true&includeAlbum=true";
+    }
+
+    public override async Task RefreshItemsAsync(ArrInstance arrInstance, ArrConfig config, HashSet<SearchItem>? items)
+    {
+        if (items?.Count is null or 0)
+        {
+            return;
+        }
+
+        Uri uri = new(arrInstance.Url, "/api/v1/command");
+
+        foreach (LidarrCommand command in GetSearchCommands(items))
+        {
+            using HttpRequestMessage request = new(HttpMethod.Post, uri);
+            request.Content = new StringContent(
+                JsonConvert.SerializeObject(command, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }),
+                Encoding.UTF8,
+                "application/json"
+            );
+            SetApiKey(request, arrInstance.ApiKey);
+
+            using HttpResponseMessage response = await _httpClient.SendAsync(request);
+            string? logContext = await ComputeCommandLogContextAsync(arrInstance, command);
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            
+                _logger.LogInformation("{log}", GetSearchLog(arrInstance.Url, command, true, logContext));
+            }
+            catch
+            {
+                _logger.LogError("{log}", GetSearchLog(arrInstance.Url, command, false, logContext));
+                throw;
+            }
+        }
+    }
+    
+    public override bool IsRecordValid(QueueRecord record)
+    {
+        if (record.ArtistId is 0 || record.AlbumId is 0)
+        {
+            _logger.LogDebug("skip | item information missing | {title}", record.Title);
+            return false;
+        }
+        
+        return base.IsRecordValid(record);
+    }
+    
+    private static string GetSearchLog(
+        Uri instanceUrl,
+        LidarrCommand command,
+        bool success,
+        string? logContext
+    )
+    {
+        string status = success ? "triggered" : "failed";
+
+        return $"album search {status} | {instanceUrl} | {logContext ?? $"albums: {string.Join(',', command.AlbumIds)}"}";
+    }
+
+    private async Task<string?> ComputeCommandLogContextAsync(ArrInstance arrInstance, LidarrCommand command)
+    {
+        try
+        {
+            if (!_loggingConfig.Enhanced)
+            {
+                return null;
+            }
+            
+            StringBuilder log = new();
+
+            List<Album>? albums = await GetAlbumsAsync(arrInstance, command.AlbumIds);
+
+            if (albums?.Count is null or 0)
+            {
+                return null;
+            }
+
+            var groups = albums
+                .GroupBy(x => x.Artist.Id)
+                .ToList();
+
+            foreach (var group in groups)
+            {
+                Album first = group.First();
+                
+                log.Append($"[{first.Artist.ArtistName} albums {string.Join(',', group.Select(x => x.Title).ToList())}]");
+            }
+
+            return log.ToString();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogDebug(exception, "failed to compute log context");
+        }
+
+        return null;
+    }
+
+    private async Task<List<Album>?> GetAlbumsAsync(ArrInstance arrInstance, List<long> albumIds)
+    {
+        Uri uri = new(arrInstance.Url, $"api/v1/album?{string.Join('&', albumIds.Select(x => $"albumIds={x}"))}");
+        using HttpRequestMessage request = new(HttpMethod.Get, uri);
+        SetApiKey(request, arrInstance.ApiKey);
+
+        using HttpResponseMessage response = await _httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+                
+        string responseBody = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<List<Album>>(responseBody);
+    }
+
+    private List<LidarrCommand> GetSearchCommands(HashSet<SearchItem> items)
+    {
+        const string albumSearch = "AlbumSearch";
+
+        return [new() { Name = albumSearch, AlbumIds = items.Select(i => i.Id).ToList() }];
+    }
+}
--- code/Infrastructure/Verticals/Jobs/GenericHandler.cs
+++ code/Infrastructure/Verticals/Jobs/GenericHandler.cs
@@ -5,7 +5,7 @@ using Domain.Models.Arr.Queue;
 using Infrastructure.Verticals.Arr;
 using Infrastructure.Verticals.DownloadClient;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Verticals.Jobs;
 
@@ -14,26 +14,32 @@ public abstract class GenericHandler : IDisposable
     protected readonly ILogger<GenericHandler> _logger;
     protected readonly SonarrConfig _sonarrConfig;
     protected readonly RadarrConfig _radarrConfig;
+    protected readonly LidarrConfig _lidarrConfig;
     protected readonly SonarrClient _sonarrClient;
     protected readonly RadarrClient _radarrClient;
+    protected readonly LidarrClient _lidarrClient;
     protected readonly ArrQueueIterator _arrArrQueueIterator;
     protected readonly IDownloadService _downloadService;
 
     protected GenericHandler(
         ILogger<GenericHandler> logger,
-        SonarrConfig sonarrConfig,
-        RadarrConfig radarrConfig,
+        IOptions<SonarrConfig> sonarrConfig,
+        IOptions<RadarrConfig> radarrConfig,
+        IOptions<LidarrConfig> lidarrConfig,
         SonarrClient sonarrClient,
         RadarrClient radarrClient,
+        LidarrClient lidarrClient,
         ArrQueueIterator arrArrQueueIterator,
         DownloadServiceFactory downloadServiceFactory
     )
     {
         _logger = logger;
-        _sonarrConfig = sonarrConfig;
-        _radarrConfig = radarrConfig;
+        _sonarrConfig = sonarrConfig.Value;
+        _radarrConfig = radarrConfig.Value;
+        _lidarrConfig = lidarrConfig.Value;
         _sonarrClient = sonarrClient;
         _radarrClient = radarrClient;
+        _lidarrClient = lidarrClient;
         _arrArrQueueIterator = arrArrQueueIterator;
         _downloadService = downloadServiceFactory.CreateDownloadClient();
     }
@@ -44,6 +50,7 @@ public abstract class GenericHandler : IDisposable
 
         await ProcessArrConfigAsync(_sonarrConfig, InstanceType.Sonarr);
         await ProcessArrConfigAsync(_radarrConfig, InstanceType.Radarr);
+        await ProcessArrConfigAsync(_lidarrConfig, InstanceType.Lidarr);
     }
 
     public virtual void Dispose()
@@ -78,6 +85,7 @@ public abstract class GenericHandler : IDisposable
         {
             InstanceType.Sonarr => _sonarrClient,
             InstanceType.Radarr => _radarrClient,
+            InstanceType.Lidarr => _lidarrClient,
             _ => throw new NotImplementedException($"instance type {type} is not yet supported")
         };
 
@@ -86,6 +94,7 @@ public abstract class GenericHandler : IDisposable
         {
             InstanceType.Sonarr => _sonarrConfig,
             InstanceType.Radarr => _radarrConfig,
+            InstanceType.Lidarr => _lidarrConfig,
             _ => throw new NotImplementedException($"instance type {type} is not yet supported")
         };
     
@@ -113,11 +122,15 @@ public abstract class GenericHandler : IDisposable
             },
             InstanceType.Sonarr when _sonarrConfig.SearchType is SonarrSearchType.Series => new SonarrSearchItem
             {
-                Id = record.SeriesId,
+                Id = record.SeriesId
             },
             InstanceType.Radarr => new SearchItem
             {
-                Id = record.MovieId,
+                Id = record.MovieId
+            },
+            InstanceType.Lidarr => new SearchItem()
+            {
+                Id = record.AlbumId
             },
             _ => throw new NotImplementedException($"instance type {type} is not yet supported")
         };
