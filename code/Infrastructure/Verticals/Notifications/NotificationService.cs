491ea7b31a53657842f5ab463adec6cb82ac207c
Flaminel <flm.marius@gmail.com>
2025-02-01 03:26:39 +0200

    revert

--- code/Infrastructure/Verticals/Notifications/NotificationService.cs
+++ code/Infrastructure/Verticals/Notifications/NotificationService.cs
@@ -14,26 +14,43 @@ public class NotificationService
         _notificationFactory = notificationFactory;
     }
 
-    public async Task Notify(Notification notification)
+    public async Task Notify(FailedImportStrikeNotification notification)
     {
         foreach (INotificationProvider provider in _notificationFactory.OnFailedImportStrikeEnabled())
         {
             try
             {
-                switch (notification)
-                {
-                    case FailedImportStrikeNotification failedMessage:
-                        await provider.OnFailedImportStrike(failedMessage);
-                        break;
-                    case StalledStrikeNotification stalledMessage:
-                        await provider.OnStalledStrike(stalledMessage);
-                        break;
-                    case QueueItemDeleteNotification queueItemDeleteMessage:
-                        await provider.OnQueueItemDelete(queueItemDeleteMessage);
-                        break;
-                    default:
-                        throw new NotImplementedException();
-                }
+                await provider.OnFailedImportStrike(notification);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogWarning(exception, "failed to send notification | provider {provider}", provider.Name);
+            }
+        }
+    }
+    
+    public async Task Notify(StalledStrikeNotification notification)
+    {
+        foreach (INotificationProvider provider in _notificationFactory.OnStalledStrikeEnabled())
+        {
+            try
+            {
+                await provider.OnStalledStrike(notification);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogWarning(exception, "failed to send notification | provider {provider}", provider.Name);
+            }
+        }
+    }
+    
+    public async Task Notify(QueueItemDeleteNotification notification)
+    {
+        foreach (INotificationProvider provider in _notificationFactory.OnQueueItemDeleteEnabled())
+        {
+            try
+            {
+                await provider.OnQueueItemDelete(notification);
             }
             catch (Exception exception)
             {
