1eacf5d5eb76f6c1830c8b2f2005a7961fc6a4f5
Flaminel <flm.marius@gmail.com>
2025-03-28 18:51:18 +0200

    added support for secrets mounted as files

---
+++ code/Infrastructure/Providers/DockerSecretsConfigurationProvider.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Providers;
+
+public sealed class DockerSecretsConfigurationProvider : ConfigurationProvider
+{
+    public override void Load()
+    {
+        foreach (object? variable in Environment.GetEnvironmentVariables())
+        {
+            if (variable is not DictionaryEntry pair)
+            {
+                continue;
+            }
+            
+            string key = pair.Key.ToString()!;
+            string? value = pair.Value?.ToString();
+
+            if (!KeyHasPattern(key))
+            {
+                continue;
+            }
+                
+            try
+            {
+                if (!string.IsNullOrEmpty(value) && File.Exists(value))
+                {
+                    Data[key] = File.ReadAllText(value).Trim();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"error loading secret for {key}", ex);
+            }
+        }
+    }
+
+    private static bool KeyHasPattern(string key)
+    {
+        if (key.EndsWith("USERNAME", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return true;
+        }
+        
+        if (key.EndsWith("PASSWORD", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return true;
+        }
+        
+        if (key.EndsWith("APIKEY", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return true;
+        }
+
+        if (key.EndsWith("API_KEY", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ code/Infrastructure/Providers/DockerSecretsConfigurationSource.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Providers;
+
+public sealed class DockerSecretsConfigurationSource : IConfigurationSource
+{
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new DockerSecretsConfigurationProvider();
+    }
+}
