b92d70769a86b3cdc1b64018c9f6b8b11cb4445f
Flaminel <flm.marius@gmail.com>
2025-05-04 15:21:41 +0300

    Add documentation (#125)

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,82 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Cleanuperr',
+  tagline: 'Cleaning arrs since \'24.',
+  favicon: 'img/16.png',
+
+  url: 'https://flmorg.github.io',
+  baseUrl: '/cleanuperr/',
+
+  organizationName: 'flmorg',
+  projectName: 'cleanuperr',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+    },
+    navbar: {
+      title: 'Cleanuperr',
+      logo: {
+        alt: 'Cleanuperr Logo',
+        src: 'img/cleanuperr.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'configurationSidebar',
+          position: 'left',
+          label: 'Docs',
+          activeBasePath: '/docs',
+        },
+        {
+          href: 'https://github.com/flmorg/cleanuperr',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          href: 'https://discord.gg/sWggpnmGNY',
+          label: 'Discord',
+          position: 'right',
+        }
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Cleanuperr. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
