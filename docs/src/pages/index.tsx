6fbae768a490587cf8e9c40bf692b1d6f9200cbb
Flaminel <flm.marius@gmail.com>
2025-05-04 15:24:31 +0300

    removed test stuff

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -34,8 +34,7 @@ export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
     >
       <HomepageHeader />
     </Layout>
